d299e72cc81cab9cc2dbb4c762aa69bfe8b87d1e
davidesigillo <sigillodav@gmail.com>
2026-03-31 22:45:21 +0200

    tolto puppa e modificato astro.config.mjs per deploy su github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mistahuman.github.io/portfolio-davidesigillo/",
+  site: "https://daddysiggi.github.io/portfolio-davidesigillo/",
   base: process.env.NODE_ENV === "production" ? "/portfolio-davidesigillo/" : "/portfolio-davidesigillo/",
   output: "static",
   integrations: [icon()]
